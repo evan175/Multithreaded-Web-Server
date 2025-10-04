d26bd05e1f1278d375721fc82eed6392c56b88e2
evan175 <ashraful.e.hoque@gmail.comgit config --global user.email ashraful.e.hoque@gmail.com>
2025-10-04 23:13:34 +0000

    first draft

--- server.c
+++ server.c
@@ -32,14 +32,7 @@ int create_socket() {
     return socket_id;
 }
 
-int bind_socket(char* ip, int port, int socket_id) {
-    struct sockaddr_in bind_args;
-    memset(&bind_args, 0, sizeof(bind_args));
-
-    bind_args.sin_family = AF_INET;
-    bind_args.sin_addr.s_addr = inet_addr(ip);
-    bind_args.sin_port = htons(port);
-
+int bind_socket(int socket_id, struct sockaddr_in bind_args) {
     int rc = bind(socket_id, (struct sockaddr*) &bind_args, sizeof(bind_args));
 
     if(rc < 0) {
@@ -63,124 +56,188 @@ int accept_conns(int socket_id) {
     return client_socket;
 }
 
-void* process_client_request(void* input) { //void pointer return so cant do exit str method to exit
-    struct thread_data *data;
-    data = (struct thread_data *) input;
+int make_connect(int socket_id, struct sockaddr_in addr){
+    int conn = connect(socket_id, (struct sockaddr*)&addr, sizeof(addr));
 
-    printf("Thread: %d, data pointer %p\n", gettid(), data);
+    if (conn < 0) {
+        perror("Connection to server failed\n");
+    }
 
-    printf("Data client socket beggining: %d\n", data->client_socket);
+    return conn;
+}
 
-    int client_socket = data -> client_socket;
-    int server_socket = data -> server_socket;
+int read_msg(int sock, char* buff) {
+    int rc = read(sock, buff, MAX_MSG_LEN - 1);// have start and end byte to see when message ends
+    if(rc < 0) {
+        printf("Error reading\n");
+    } else {
+        buff[rc] = '\0';
+    }
+
+    return rc;
+}
+
+int write_msg(int sock, char* buff) {
+    int wc = write(sock, buff, strlen(buff));
+
+    if(wc < 0) {
+        printf("Error writing\n");
+    }
+
+    return wc;
+}
+
+int check_buff(char* buff) {
+    size_t len = strlen(buff);
+
+    // Check if the read line contains '\n' => means user typed less than max len
+    if (len > 0 && buff[len - 1] == '\n') {
+        buff[len - 1] = '\0';  
+        len -= 1; // Adjust length to exclude the newline
+
+        return 1;
+    } else {
+        // No '\n' => the user typed at least MAX_MSG_LEN+1 chars before hitting Enter:
+        // We must flush the rest of the line from stdin.
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF) {
+            
+        }
+        printf("Error: input exceeds %d characters. Please try again.\n", MAX_MSG_LEN);
+        
+        return -1;
+    }
+}
+
+void* recv_msg(void* input) {
+    int* sock_id;
+    sock_id = (int*) input;
+
+    // printf("Thread: %d, data pointer %p\n", gettid(), input_data);
 
     char buff[MAX_MSG_LEN];
     memset(buff, 0, sizeof(buff));
-    int rc = read(client_socket, buff, MAX_MSG_LEN);
-    if(rc < 0) {
-        printf("Error reading from client");
-    }
-
-    char method[8], operation[64], version[16];
-
-    // Assuming buff contains the request string (like "GET /Exit HTTP/1.1")
-    sscanf(buff, "%s %s %s", method, operation, version);
     
-    if(strcmp(operation, "/Exit") == 0) {
-        printf("exit recieved\n");
-        close(client_socket);
-        close(server_socket);
-        return NULL;
+    while(1) {
+        int rc = read_msg(*sock_id, buff);
+        // printf("rc: %i\n", rc);
+        if(rc == 0) {
+            printf("Connection has closed\n");
+            return NULL;
+        }
+
+        printf("Message Received: : %s\n", buff);  
+    }
+}
+
+void* send_msg(void* input) {
+    struct thread_data *input_data;
+    input_data = (struct thread_data *) input;
+
+    // printf("Thread: %d, data pointer %p\n", gettid(), input_data);
+
+    int client_socket = input_data -> client_socket;
+    int server_socket = input_data -> server_socket;
+
+    char buff[MAX_MSG_LEN];
+    memset(buff, 0, sizeof(buff));
+
+    while(1) {
+        if (fgets(buff, sizeof(buff), stdin) == NULL) { //send 30 if more than 30, then send remaining message
+            // EOF or read error
+            printf("\nEnd of input. Exiting.\n");
+            break;
+        }
+
+        if(check_buff(buff) < 0) {
+            continue;
+        }
+
+        if(strcmp(buff, "Exit") == 0) {
+            printf("Exiting\n");
+            
+            if(server_socket) {
+                close(server_socket);
+                printf("Closed Server Socket\n");
+                return NULL;
+            }
+
+            close(client_socket);
+            printf("Closed client socket\n");
+
+            return NULL;
+        }
+
+        write_msg(client_socket, buff);
+
+        memset(buff, 0, sizeof(buff));
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s [server|client] <ip> <port>\n", argv[0]);
+        return 1;
     }
 
-    printf("From client: %s\n", operation);
+    char* mode = argv[1];
+    char* ip = argv[2];
+    int port = atoi(argv[3]);
 
-    int num1;
-    int num2;
-    int summ;
-    char html_reply[MAX_REPLY_LEN];
+    int socket_id = create_socket();
 
-    if (sscanf(operation, "/add_%d_%d", &num1, &num2) == 2) {
-        summ = num1 + num2;
+    struct sockaddr_in bind_args;
+    memset(&bind_args, 0, sizeof(bind_args));
+
+    bind_args.sin_family = AF_INET;
+    bind_args.sin_addr.s_addr = inet_addr(ip);
+    bind_args.sin_port = htons(port);
+
+    struct thread_data *td = malloc(sizeof(struct thread_data)); 
+
+    if(strcmp(mode, "server") == 0) {
+        bind_socket(socket_id, bind_args);
+
+        if((listen(socket_id, 3)) < 0) { //stop program here if error
+            printf("error listening\n");
+            printf("Errno: %d\n", errno);
+        }
+
+        int client_socket = accept_conns(socket_id);
+
+        td->client_socket = client_socket;
+        td->server_socket = socket_id;
+
+        if(client_socket > 0) {
+            printf("Connected\n");
+        }
+    }
+
+    if(strcmp(mode, "client") == 0) {
+        if(make_connect(socket_id, bind_args) >= 0){
+            printf("Connected\n");
+        }
+
         
-        snprintf(html_reply, MAX_REPLY_LEN, "HTTP/1.1 200 OK\r\n"
-          "Content-Type: text/html; charset=UTF-8\r\n"
-          "Connection: keep-alive\r\n"
-          "\r\n"
-          "<!DOCTYPE html>"
-          "<html><body>%d + %d = %d</body></html>", num1, num2, summ);
-        
-        write(client_socket, html_reply, strlen(html_reply));
-        //read again to see if client send something else (it doesnt)
-        // int rc = read(client_socket, buff, MAX_MSG_LEN);
-        // if(rc < 0) {
-        //     printf("Error reading from client 2");
-        // }
-        // printf("from client 2: %s\n", buff)
-    } else {
-        printf("Failed to parse the string.\n");
-        snprintf(html_reply, MAX_REPLY_LEN, "HTTP/1.1 400 BAD REQUEST\r\n"
-        "Content-Type: text/html; charset=UTF-8\r\n"
-        "\r\n"
-        "<!DOCTYPE html>"
-        "<html><body>Invalid Request</body></html>");
 
-        write(client_socket, html_reply, strlen(html_reply));
+        td->client_socket = socket_id;
     }
 
-    sleep(10);
+    pthread_t read_thread;
+    pthread_t write_thread;
 
-    printf("Data client socket end: %d\n", data->client_socket);
+    pthread_create(&write_thread, NULL, send_msg, (void *) td);
+    pthread_create(&read_thread, NULL, recv_msg, (void *) &td->client_socket);
 
-    if(close(client_socket) < 0) {
-        printf("error closing client socket");
-    }
+    pthread_join(write_thread, NULL);
+    pthread_join(read_thread, NULL);
 
-    free(data);
-} //break down this func in different routines
-
-
-int main() {
-   int socket_id = create_socket();
-
-   int bind_val = bind_socket("127.0.0.1", 80, socket_id);
-
-   if((listen(socket_id, 3)) < 0) {
-    printf("error listening\n");
-    printf("Errno: %d\n", errno);
-   }
-
-   while(1) {
-    struct thread_data *td = malloc(sizeof(struct thread_data)); //if not use malloc this will be used by multiple threads. mallac keeps var alive after while loope finishe
-    td->server_socket = socket_id;
-
-    int client_socket = accept_conns(socket_id);
-
-    if(errno == 9) { //when str "Exit" is recieved
-        printf("Server socket closed or error with client socket\n");
-        break;
-    } 
-
-    td->client_socket = client_socket;
-
-    if(client_socket < 0) {
-        printf("Error accepting client connection");
-    }
-
-    pthread_t thread1;
-
-    pthread_create(&thread1, NULL, process_client_request, (void *) td);
-
-    // if((process_client_request(client_socket, socket_id)) == -1 ) { //exit str
-    //     break;
-    // }
-    
-   }
-   printf("exiting\n");
+    printf("exiting\n");
 
 }
 
-//make chat system
-//pass in command line ip + port who to talk to
-//one thread that reads and one the writes to other side
+//Exit str doesnt end other connection
 //
+//protocal for sending msg, STX len maybe checksum (no need for ETX bc of len)
+//if len is < 255 send one byte as its val
